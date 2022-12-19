1f5070821f8ed9cf1799eb8e190c2cecd16c7d6c
jvillad <jyvillad@gmail.com>
2022-12-19 22:45:18 +1100

    additonal configs to complete template

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={120000} position="Junior SDE" company="Google" />
-      <Job salary={100000} position="Project Manager" company="Netflix" />
-    </div>
-  );
-}
+function App() {}
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h2>{props.company}</h2>
-    </div>
-  );
-};
 export default App;
