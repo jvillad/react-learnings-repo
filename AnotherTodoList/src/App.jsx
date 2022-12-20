c03ddb116e8f0f77cab3768d6969e3965ca0f23c
jvillad <jyvillad@gmail.com>
2022-12-20 21:45:11 +1100

    added a delete task feature

--- AnotherTodoList/src/App.jsx
+++ AnotherTodoList/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
+
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
@@ -10,7 +11,11 @@ function App() {
   };
 
   const addTask = () => {
-    setTodoList([...todoList, newTask]);
+    const task = {
+      id: Date.now(),
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
   };
 
   // TODO: make a new component
@@ -18,23 +23,22 @@ function App() {
     return todoList.map((task, key) => {
       return (
         <div key={key}>
-          <h1>{task}</h1>
-          <button>X</button>
+          <h1>{task.taskName}</h1>
+          <button onClick={() => deleteTask(task.id)}>X</button>
         </div>
       );
     });
   };
-  // TODO
-  // const deleteTask = () => {
 
-  // }
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
 
   return (
     <div className="App">
       <div className="addTask"></div>
       <input type="text" onChange={handleChange} />
       <button onClick={addTask}> Add Task </button>
-      <div className="list">{renderTask()}</div>
       <div className="list">{renderTask()}</div>
     </div>
   );
