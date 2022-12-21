f277e5a348201ea72b01f645ae0ac00ca06733df
jvillad <jyvillad@gmail.com>
2022-12-22 10:04:53 +1100

    Wrapping App to QueryClientProvider

--- simple-catfact/src/main.jsx
+++ simple-catfact/src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
