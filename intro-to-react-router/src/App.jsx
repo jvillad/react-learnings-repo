978657e5ed89114089ed611258141c25284996d3
jvillad <jyvillad@gmail.com>
2022-12-23 20:42:59 +1100

    basic navbar to navigate

--- intro-to-react-router/src/App.jsx
+++ intro-to-react-router/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
