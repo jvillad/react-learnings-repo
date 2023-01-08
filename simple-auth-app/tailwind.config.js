535a5320bd9bf1af248b35b18e70908b0bbba6ef
jvillad <jyvillad@gmail.com>
2023-01-09 09:04:18 +1100

    update config, added color variable

--- simple-auth-app/tailwind.config.js
+++ simple-auth-app/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "boon-pink": "#f353b2",
+      },
+    },
   },
   plugins: [],
 };
