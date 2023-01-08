f4bc736f27b64aefce0099346a97d3c197133c7e
jvillad <jyvillad@gmail.com>
2023-01-08 20:57:43 +1100

    link layout and component

--- simple-auth-app/pages/_app.js
+++ simple-auth-app/pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component }) {
+  return (
+    <Layout>
+      <Component />
+    </Layout>
+  );
 }
