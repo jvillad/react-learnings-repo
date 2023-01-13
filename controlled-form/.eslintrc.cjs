e01dfae37afeb18e95a812896277c912e3e9f8fe
jvillad <jyvillad@gmail.com>
2023-01-13 14:16:12 +1100

    form initial commit

---
+++ controlled-form/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: { 'react/react-in-jsx-scope': 'off' },
+};
