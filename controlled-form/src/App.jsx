de14ee80b8025c89a1081a0d58c94f422ad783e7
jvillad <jyvillad@gmail.com>
2023-01-14 00:24:58 +1100

    client provider

--- controlled-form/src/App.jsx
+++ controlled-form/src/App.jsx
@@ -1,6 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Form from './components/Form';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 function App() {
-  return <Form />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Form />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
