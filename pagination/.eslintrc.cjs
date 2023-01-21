97069ef9c6ef884426f328b8f99a9c1908f9a0cc
jvillad <jyvillad@gmail.com>
2023-01-21 16:07:10 +1100

    delete test, update eslint

--- pagination/.eslintrc.cjs
+++ pagination/.eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: 'pagination/tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
