e9052e1ae8ae7880bcf11607a361b2275cf5400e
jvillad <jyvillad@gmail.com>
2023-01-21 16:07:16 +1100

    fetch items

--- pagination/src/App.tsx
+++ pagination/src/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [theData, setTheData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchItems = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://api.guildwars2.com/v2/items`);
+      const data = await response.json();
+      if (data) {
+        setTheData(data);
+      }
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={fetchItems}>
+        Click me
+      </button>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {theData && <p>{theData}</p>}
+    </div>
+  );
 }
 
 export default App;
