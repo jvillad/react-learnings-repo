e34200025dcc5d394e04469458871428dbbd2a7f
jvillad <jyvillad@gmail.com>
2023-03-27 22:16:08 +1100

    import title component to App

--- dnd-react/src/App.tsx
+++ dnd-react/src/App.tsx
@@ -1,11 +1,12 @@
 // import { useState } from "react";
 import "./App.css";
+import { Title } from "./components/Title";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="container-main flex">
+      <Title />
+    </div>
   );
 }
 
