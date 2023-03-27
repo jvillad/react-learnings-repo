a3e6a8d209efdd56820b94287241e7a5117d7b5c
jvillad <jyvillad@gmail.com>
2023-03-27 21:59:59 +1100

    cleanup

--- dnd-react/src/components/Box.tsx
+++
@@ -1,9 +0,0 @@
-import Draggable from "./Draggable";
-
-export const Box = ({ hasItem }) => {
-  return (
-    <div className="flex w-[100px] h-[100px] justify-center align-middle">
-      {hasItem && <Draggable />}
-    </div>
-  );
-};
--- dnd-react/src/components/Draggable.tsx
+++
@@ -1,4 +0,0 @@
-const Draggable = () => {
-  return <div>Drag me please!</div>;
-};
-export default Draggable;
