c0bd380d4d5cab1cbf352e489f8f2e8c929bb123
jvillad <jyvillad@gmail.com>
2023-03-28 22:32:32 +1100

    import drag and drop component

--- dnd-react/src/App.tsx
+++ dnd-react/src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 import "./App.css";
+import { DragAndDrop } from "./components/DragAndDrop";
 import { Title } from "./components/Title";
 
 function App() {
   return (
     <div className="container-main flex">
       <Title />
+      <DragAndDrop />
     </div>
   );
 }
