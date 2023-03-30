022a80e163697b45a6115b1276e1e9de0e9765b3
jvillad <jyvillad@gmail.com>
2023-03-30 23:41:29 +1100

    ondragover and ondrop

--- dnd-react/src/components/CardContainer.tsx
+++ dnd-react/src/components/CardContainer.tsx
@@ -1,9 +1,36 @@
-import { IProps } from "../interfaces/interface";
+import { CardContainerProps } from "../interfaces/interface";
+import { Card } from "./Card";
 
-export const CardContainer = ({ status }: IProps) => {
+export const CardContainer = ({
+  items = [],
+  status,
+  isDragging,
+  handleDragging,
+  handleUpdateList,
+}: CardContainerProps) => {
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const id = +e.dataTransfer.getData("text");
+    handleUpdateList(id, status);
+    handleDragging(false);
+  };
   return (
-    <div className="layout-cards">
+    <div
+      className={`layout-cards ${isDragging ? "layout-dragging" : ""}`}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
       <p>Tier {status}</p>
+      {items.map(
+        (item) =>
+          status === item.status && (
+            <Card data={item} key={item.id} handleDragging={handleDragging} />
+          )
+      )}
     </div>
   );
 };
