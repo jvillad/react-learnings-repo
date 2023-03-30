68b318305e4205862b05e01fd89fed8b1a122fd8
jvillad <jyvillad@gmail.com>
2023-03-30 23:36:50 +1100

    seed

---
+++ dnd-react/src/assets/Seed.ts
@@ -0,0 +1,24 @@
+import { IData } from "../interfaces/interface";
+
+export const dataSeed: IData[] = [
+  {
+    id: 1,
+    content: "Nozawa Onsen",
+    status: "S",
+  },
+  {
+    id: 2,
+    content: "Akakura Onsen",
+    status: "A",
+  },
+  {
+    id: 3,
+    content: "Hakuba Valley",
+    status: "S",
+  },
+  {
+    id: 4,
+    content: "Shiga Kogen",
+    status: "B",
+  },
+];
