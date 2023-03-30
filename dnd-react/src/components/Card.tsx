3b1fa86482e757a747b3d4954193ca8e1cc4b663
jvillad <jyvillad@gmail.com>
2023-03-30 23:40:52 +1100

    add 2 function to handle dragstart and end

--- dnd-react/src/components/Card.tsx
+++ dnd-react/src/components/Card.tsx
@@ -1,7 +1,19 @@
-export const Card = () => {
+import { ItemProps } from "../interfaces/interface";
+
+export const Card = ({ data, handleDragging }: ItemProps) => {
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    e.dataTransfer.setData("text", `${data.id}`);
+    handleDragging(true);
+  };
+  const handleDragEnd = () => handleDragging(false);
   return (
-    <div className="card-container">
-      <p>content</p>
+    <div
+      className="card-container"
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <p>{data.content}</p>
     </div>
   );
 };
