782e909cb74dd7a1a90ac3c83ec6a9b38bd3a634
jvillad <jyvillad@gmail.com>
2023-04-22 23:25:51 +1000

    setting up routing

--- react-vite-redux/src/App.tsx
+++ react-vite-redux/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
+import Machines from './components/Machines';
+import About from './components/about/About';
+import Nav from './components/common/Nav';
+import PageNotFound from './components/common/PageNotFound';
+import Home from './components/home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <div className="bg-black">
-        <h1 className="text-white">Text White</h1>
-      </div>
-    </>
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/machines" element={<Machines />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
