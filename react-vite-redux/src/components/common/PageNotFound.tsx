446a78aa609b60023ae15ea079daab3cce2cc8e1
jvillad <jyvillad@gmail.com>
2023-04-22 23:26:33 +1000

    page not found

---
+++ react-vite-redux/src/components/common/PageNotFound.tsx
@@ -0,0 +1,11 @@
+const PageNotFound = () => {
+  return (
+    <div className="container lg:max-w-screen-lg mx-auto">
+      <h1 className="flex justify-center align-middle font-semibold text-[34px] py-20 text-gray-800">
+        Oopsie! Page not found
+      </h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
