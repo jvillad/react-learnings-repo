b7c4081ea865c28d621fce7c2e4d89797f0099e2
jvillad <jyvillad@gmail.com>
2023-04-22 23:29:35 +1000

    machine page

---
+++ react-vite-redux/src/components/Machines.tsx
@@ -0,0 +1,9 @@
+const Machines = () => {
+  return (
+    <div className="container lg:max-w-screen-lg mx-auto text-center py-10">
+      <h1 className="text-orange-700 text-xl">Our Goated Coffee Machines</h1>
+    </div>
+  );
+};
+
+export default Machines;
