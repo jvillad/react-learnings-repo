83cddbc630f8fa9426baededc1560903bdea26d1
jvillad <jyvillad@gmail.com>
2023-04-23 23:01:23 +1000

    item demo dashboard

---
+++ react-vite-redux/src/components/Items.tsx
@@ -0,0 +1,13 @@
+import MachineList from './MachineList';
+import Machines from './Machines';
+
+const Items = () => {
+  return (
+    <div>
+      <Machines />
+      <MachineList />
+    </div>
+  );
+};
+
+export default Items;
