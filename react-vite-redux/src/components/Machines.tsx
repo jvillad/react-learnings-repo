86784b6a0bcdbebed81896288cf55da26322a107
jvillad <jyvillad@gmail.com>
2023-04-23 23:02:17 +1000

    change hook to useRef to avoid re-renders

--- react-vite-redux/src/components/Machines.tsx
+++ react-vite-redux/src/components/Machines.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
+import { useRef } from 'react';
+import { useAppDispatch } from '../redux/store/store';
+import { addItem } from '../redux/store/features/itemSlice';
 
 const Machines = () => {
-  const [item, setItem] = useState({ name: '' });
+  const name = useRef<string>('');
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setItem({ name: value });
-  };
+  const dispatch = useAppDispatch();
 
   return (
     <div className="container lg:max-w-screen-lg mx-auto  py-10">
-      <form onSubmit={() => alert(item.name)}>
-        <h3 className="text-orange-700 text-xl text-center">Add Item</h3>
-        <input
-          className="text-[#0a0a0a]"
-          type="text"
-          onChange={handleChange}
-          value={item.name}
-        />
-        <input type="submit" value="Save" className="block" />
-      </form>
+      <h3 className="text-orange-700 text-xl text-center">Add Item</h3>
+      <label htmlFor="" className="p-1">
+        Item Name
+      </label>
+      <input
+        className="text-[#0a0a0a] block p-1"
+        type="text"
+        onChange={(e) => (name.current = e.target.value)}
+      />
+      <button
+        type="submit"
+        className="block"
+        onClick={() => dispatch(addItem({ name: name.current }))}
+      >
+        Add
+      </button>
     </div>
   );
 };
